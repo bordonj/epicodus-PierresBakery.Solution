5940dd5083878d4c0f8a042363485488a38e2f4b
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-16 09:54:39 -0700

    add logic to pass test checking pastry amount

--- PierresBakery/Models/PastryBread.cs
+++ PierresBakery/Models/PastryBread.cs
@@ -2,14 +2,26 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-    // properties, methods, etc. will go here.
+    // Public properties
+    public int Amount { get; set; }
+
+    // Constructor
+    public Pastry(int amount)
+    {
+      Amount = amount;
+      int price = 0;
+    }
   }
 
-
+    // properties, methods, etc. will go here.
   
   public class Bread
   {
-    // properties, methods, etc. will go here.
+    // Public properties
+
+    // Constructor
+
+    }
   }
 }
 
