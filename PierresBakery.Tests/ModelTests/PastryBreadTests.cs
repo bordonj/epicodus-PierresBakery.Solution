9b5ecd316e632ded2868ddef659b2cd0375ce702
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-16 10:05:48 -0700

    add test to check amount of bread

--- PierresBakery.Tests/ModelTests/PastryBreadTests.cs
+++ PierresBakery.Tests/ModelTests/PastryBreadTests.cs
@@ -31,7 +31,20 @@ namespace PierresBakery.Tests
         //Assert
         Assert.AreEqual(1, newPastry.Amount);
       }
-    // Test methods will go here.
+      [TestMethod]
+      public void SetAmountOfBreadToBuy_SetAmount_Int()
+      {
+        //Arrange
+        int breadAmt = 1;
+
+        //Act
+        Bread newBread = new Bread(breadAmt);
+        // Item newItem = new Item(description);
+
+        //Assert
+        Assert.AreEqual(1, newBread.Amount);
+      }
+
 
   }
 }
