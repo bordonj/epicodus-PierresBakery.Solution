c754ef0d9877a0e060cdf711dc81bcd79346c395
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-16 09:51:38 -0700

    add test for checking pastry amount

--- PierresBakery.Tests/ModelTests/PastryBreadTests.cs
+++ PierresBakery.Tests/ModelTests/PastryBreadTests.cs
@@ -9,15 +9,28 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+      [TestMethod]
+      public void SetAmountOfPastriesToBuy_SetAmount_Int()
+      {
+        //Arrange
+        int pastryAmt = 1;
+
+        //Act
+        Pastry newPastry = new Pastry(pastryAmt);
+        // Item newItem = new Item(description);
+
+        //Assert
+        Assert.AreEqual(1, newPastry.Amount);
+      }
     // Test methods will go here.
 
   }
